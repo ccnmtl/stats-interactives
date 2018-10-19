ce4fcb89d43071d804d8795fabb60d979f4ea04f
Nick Buonincontri <nbuonin@gmail.com>
2018-10-19 17:12:00 -0400

    WIP

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
     entry: "./src/index.js",
@@ -20,5 +21,11 @@ module.exports = {
                 loader: ['babel-loader']
             }
         ]
-    }
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: path.resolve(__dirname, './src/index.html'),
+            filename: './src/index.html'
+        })
+    ]
 }
