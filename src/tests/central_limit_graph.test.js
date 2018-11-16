54736691985b28d720b84e21e21d616bf80dfaa3
Nick Buonincontri <nbuonin@gmail.com>
2018-11-16 14:36:09 -0500

    Write tests for seed functionality
    This commit implements tests to verify that the same seed consistently produces the same population and sample data.

--- src/tests/central_limit_graph.test.js
+++ src/tests/central_limit_graph.test.js
@@ -83,3 +83,82 @@ describe('Conditionally show the nav based on embed query string param', () => {
         expect(wrapper.find('Nav').exists()).toEqual(true);
     });
 })
+
+describe('Ensure that the same seed generates the same population and samples', () => {
+    test('The same seed generates the same population', () => {
+        // render the graph with a seed, save the pop values
+        // Render it with a different seed, test that its different
+        // Render it again with the first seed and check its the same as the first
+
+        const wrapper = mount(
+            <MemoryRouter>
+                <CentralLimitGraph />
+            </MemoryRouter>
+        );
+        // Render same data with a seed
+        wrapper.find('CentralLimitGraph').setState({seed: 'my-new-seed'});
+        expect(wrapper.find('#seed').props().value).toEqual('my-new-seed');
+        let pop1 = wrapper.find('CentralLimitGraph').state('population');
+        let popData1 = wrapper.find('CentralLimitGraph').state('populationGraphData');
+
+        // Render it with a different seed
+        wrapper.find('CentralLimitGraph').setState({seed: 'a-different-seed'});
+        expect(wrapper.find('#seed').props().value).toEqual('a-different-seed');
+        let pop2 = wrapper.find('CentralLimitGraph').state('population');
+        let popData2 = wrapper.find('CentralLimitGraph').state('populationGraphData');
+
+        // Render it again with the same seed as the first time
+        wrapper.find('CentralLimitGraph').setState({seed: 'my-new-seed'});
+        expect(wrapper.find('#seed').props().value).toEqual('my-new-seed');
+        let pop3 = wrapper.find('CentralLimitGraph').state('population');
+        let popData3 = wrapper.find('CentralLimitGraph').state('populationGraphData');
+
+        // Expect that data generated from different seeds is different
+        expect(pop1).not.toContainEqual(pop2);
+        expect(popData1).not.toEqual(popData2);
+
+        // Expect that data generated from the same seed is the same
+        expect(pop1).toContainEqual(pop3);
+        expect(popData1).toEqual(popData3);
+    });
+
+    test('The same seed generates the same samples', () => {
+        // render the graph with a seed, save the pop values
+        // Render it with a different seed, test that its different
+        // Render it again with the first seed and check its the same as the first
+
+        const wrapper = mount(
+            <MemoryRouter>
+                <CentralLimitGraph />
+            </MemoryRouter>
+        );
+        // Render same data with a seed
+        wrapper.find('CentralLimitGraph').setState({seed: 'my-new-seed'});
+        wrapper.find('CentralLimitGraph').instance().runSample();
+        expect(wrapper.find('#seed').props().value).toEqual('my-new-seed');
+        let sample1 = wrapper.find('CentralLimitGraph').state('sampleMeans');
+        let sampleData1 = wrapper.find('CentralLimitGraph').state('sampleMeansGraphData');
+
+        // Render it with a different seed
+        wrapper.find('CentralLimitGraph').setState({seed: 'a-different-seed'});
+        wrapper.find('CentralLimitGraph').instance().runSample();
+        expect(wrapper.find('#seed').props().value).toEqual('a-different-seed');
+        let sample2 = wrapper.find('CentralLimitGraph').state('sampleMeans');
+        let sampleData2 = wrapper.find('CentralLimitGraph').state('sampleMeansGraphData');
+
+        // Render it again with the same seed as the first time
+        wrapper.find('CentralLimitGraph').setState({seed: 'my-new-seed'});
+        wrapper.find('CentralLimitGraph').instance().runSample();
+        expect(wrapper.find('#seed').props().value).toEqual('my-new-seed');
+        let sample3 = wrapper.find('CentralLimitGraph').state('sampleMeans');
+        let sampleData3 = wrapper.find('CentralLimitGraph').state('sampleMeansGraphData');
+
+        // Expect that data generated from different seeds is different
+        expect(sample1).not.toContainEqual(sample2);
+        expect(sampleData1).not.toEqual(sampleData2);
+
+        // Expect that data generated from the same seed is the same
+        expect(sample1).toContainEqual(sample3);
+        expect(sampleData1).toEqual(sampleData3);
+    })
+});
