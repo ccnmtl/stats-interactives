6672916dc646854e304a1519ae9a1fd4a15015e9
Nick Buonincontri <nbuonin@gmail.com>
2019-02-18 15:42:15 -0500

    Setup Google Analytics

---
+++ src/withTracker.jsx
@@ -0,0 +1,40 @@
+/* eslint-disable */
+/**
+ * From ReactGA Community Wiki Page https://github.com/react-ga/react-ga/wiki/React-Router-v4-withTracker
+ */
+
+import React, { Component } from 'react';
+import ReactGA from 'react-ga';
+
+ReactGA.initialize('UA-51144540-29');
+
+export default function withTracker(WrappedComponent, options = {}) {
+    const trackPage = (page) => {
+        ReactGA.set({
+            page,
+            ...options
+        });
+        ReactGA.pageview(page);
+    };
+
+    const HOC = class extends Component {
+        componentDidMount() {
+            const page = this.props.location.pathname;
+            trackPage(page);
+        }
+
+
+        componentDidUpdate(prevProps) {
+            const currentPage =
+                prevProps.location.pathname + prevProps.location.search;
+            const nextPage =
+                this.props.location.pathname + this.props.location.search;
+
+        }
+        render() {
+            return <WrappedComponent {...this.props} />;
+        }
+    };
+
+    return HOC;
+}
