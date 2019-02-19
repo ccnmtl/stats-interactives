4a4c86e8d3d0b515d82af3e6b2529cd3bb2353ae
Nick Buonincontri <nbuonin@gmail.com>
2019-02-19 12:21:50 -0500

    Update Graph Labels
    This commit:
    - Removes the VictoryLegend component
    - Wraps the graphs in a col, adds sticky-top to this container
    - Renders the graph titles as HTML inside the above container

--- src/central_limit_theorem/SampleMeansGraph.jsx
+++ src/central_limit_theorem/SampleMeansGraph.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import {
     VictoryChart, VictoryTheme, VictoryAxis,
-    VictoryScatter, VictoryLabel, VictoryLegend } from 'victory';
+    VictoryScatter, VictoryLabel} from 'victory';
 import * as math from 'mathjs';
 math.config({matrix: 'Array'});
 import { MIN_BIN, MAX_BIN } from './CentralLimitGraph';
@@ -18,16 +18,18 @@ export const SampleMeansGraph = ({
     return (
         <>
         <VictoryChart theme={VictoryTheme.material}
-            padding={{top: 0, left: 5, right: 5, bottom: 25}}
+            padding={{top: 0, left: 10, right: 10, bottom: 25}}
             width={850}
+            height={350}
+            style={{
+                parent: {
+                    height: 'inherit',
+                    padding: '0.5em 1em 1em 1em',
+                }
+            }}
             domain={{x: [MIN_BIN, MAX_BIN], y: range}}>
             <VictoryAxis
                 tickValues={math.range(MIN_BIN, MAX_BIN, true)} />
-            <VictoryLegend
-                title='Distribution of Sample Means'
-                style={{ title: { fontSize: '28px' },
-                    data: { display: 'none' },
-                    labels: { display: 'none' }}}/>
             {sampleMeansGraphData &&
                 <VictoryScatter data={sampleMeansGraphData}
                     x={0}
