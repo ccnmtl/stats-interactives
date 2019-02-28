6136e2f980def590ab2ae9aab9701b4ce6b585c6
Nick Buonincontri <nbuonin@gmail.com>
2019-02-28 17:02:22 -0500

    COJ-159 Add labels and ticks to tax rate slider

--- src/ordinary_least_squares/TaxRateSlider.jsx
+++ src/ordinary_least_squares/TaxRateSlider.jsx
@@ -2,6 +2,37 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import Rheostat from 'rheostat';
 
+const TaxRatePitComponent = ({ style, children }) => {
+    return (
+        <div
+            style={{
+                ...style,
+                background: '#a2a2a2',
+                width: 1,
+                height: 12,
+                top: 10,
+                display: 'flex',
+                justifyContent: 'center'
+            }}
+        >
+            <div style={{marginTop: 16}}>
+                {((children) => {
+                    switch (children) {
+                    case 1:
+                        return '3%';
+                    case 25:
+                        return '5%';
+                    case 50:
+                        return '7%';
+                    case 75:
+                        return '7.5%';
+                    }
+                })(children)}
+            </div>
+        </div>
+    );
+};
+
 export const TaxRateSlider = ({taxRateIdx, handleTaxRateIdx}) => {
     return (
         <form className="tax-rate-slider">
@@ -14,7 +45,8 @@ export const TaxRateSlider = ({taxRateIdx, handleTaxRateIdx}) => {
                                 max={100}
                                 values={[taxRateIdx ?
                                     taxRateIdx : 1]}
-                                snap={true}
+                                pitComponent={TaxRatePitComponent}
+                                pitPoints={[1, 25, 50, 75]}
                                 onValuesUpdated={(sliderState) => {
                                     handleTaxRateIdx(
                                         sliderState.values[0]);
@@ -25,6 +57,11 @@ export const TaxRateSlider = ({taxRateIdx, handleTaxRateIdx}) => {
             </fieldset>
         </form>
     );
+};
+
+TaxRatePitComponent.propTypes = {
+    style: PropTypes.object,
+    children: PropTypes.number
 };
 
 TaxRateSlider.propTypes = {
