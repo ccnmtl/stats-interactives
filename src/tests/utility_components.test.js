81ecebc5d42079ff3dbf00b3fda787e84e341121
Nick Buonincontri <nbuonin@gmail.com>
2019-03-19 16:13:24 -0400

    COJ-191 Unit test keyUp functionality

--- src/tests/utility_components.test.js
+++ src/tests/utility_components.test.js
@@ -52,10 +52,29 @@ describe('The NumericField componenet', () => {
     });
 
     test('that the state of the component is updated when value prop is updated', () => {
-        const wrapper = shallow(<NumericField min={1} max={100} value={42}/>);
+        const wrapper = mount(<NumericField min={1} max={100} value={42}/>);
         const instance = wrapper.instance();
 
         wrapper.setProps({value: 15})
         expect(wrapper.state('value')).toEqual(15);
+    });
+
+    test('that pressing the up or down button updates the value', () => {
+        // We don't need to test that the value is updated, just that the
+        // expected handler is called.
+        const wrapper = shallow(<NumericField min={1} max={100} value={42}/>);
+        const instance = wrapper.instance();
+
+        // set up a dummy func
+        instance.setValue = (e) => { return; };
+        jest.spyOn(instance, 'setValue');
+
+        // test the up arrow
+        wrapper.find('input').simulate('keyUp', {keyCode: 38});
+        expect(instance.setValue).toHaveBeenCalled();
+
+        // test the down arrow
+        wrapper.find('input').simulate('keyUp', {keyCode: 40});
+        expect(instance.setValue).toHaveBeenCalled();
     });
 });
