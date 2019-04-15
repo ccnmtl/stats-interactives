3954aa388ad042775c80895a757d2bdd96697835
Nick Buonincontri <nbuonin@gmail.com>
2019-04-15 12:34:07 -0400

    COJ-235 Least Squares Color trial

--- src/colors.js
+++ src/colors.js
@@ -2,5 +2,5 @@ export const BAR_FILL = '#007CAB';
 export const BAR_BORDER = '#2C2C2C';
 export const INDICATOR = '#FFC800';
 export const INACTIVE = '#a2a2a2';
-export const LEAST_SQAURES_EST = '#00CCFF';
-export const LEAST_SQAURES_OPT = '#FFFF66';
+export const LEAST_SQAURES_EST = '#1C99DA';
+export const LEAST_SQAURES_OPT = '#F38019';
