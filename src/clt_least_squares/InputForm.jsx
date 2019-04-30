9c2cea0fe2a36aa1c00bf0ae828742c4cd4167ad
Nick Buonincontri <nbuonin@gmail.com>
2019-04-30 13:07:37 -0400

    COJ-286 Add sticky scroll

--- src/clt_least_squares/InputForm.jsx
+++ src/clt_least_squares/InputForm.jsx
@@ -17,191 +17,192 @@ export const InputForm = ({seed, handleSeed, handleGeneratePop, beta,
         handleGeneratePop();
     };
     return (
-        <>
-        <form onSubmit={handleGenPop}
-            className="needs-validation clt-least-squares-form" noValidate >
-            <fieldset>
-                <div className="form-group">
-                    <div className="form-row">
-                        <label htmlFor="seed" className="float-right">
-                            Seed:</label>
-                        <span className="help-tooltip"
-                            tabIndex="0"
-                            data-tip
-                            data-for="seed-tt">
-                            <sup>
-                                <i className="fas fa-question-circle"></i>
-                            </sup>
-                        </span>
-                        <ReactTooltip id="seed-tt" event="focus"
-                            eventOff="blur">
-                            <span>Enter a different value to see a
-                                different simulation</span>
-                        </ReactTooltip>
-                    </div>
-                    <div>
-                        <input type="text"
-                            id="seed"
-                            className={
-                                seed ? 'form-control is-valid' :
-                                    'form-control' }
-                            value={seed}
-                            onChange={hndlSeed}
-                            placeholder={
-                                'Enter a seed to start the simulation'}
-                            autoFocus required/>
-                    </div>
-                </div>
-                { seed &&
-                <div className={'form-group'}>
-                    <div className={'form-row'}>
-                        <label htmlFor={'alpha'}>
-                            <InlineMath>
-                                {String.raw`\beta_0`}
-                            </InlineMath>
-                        </label>
-                        <span className="help-tooltip"
-                            tabIndex="0"
-                            data-tip
-                            data-for="alpha-tt">
-                            <sup>
-                                <i className="fas fa-question-circle"></i>
-                            </sup>
-                        </span>
-                        <ReactTooltip id="alpha-tt" event="focus"
-                            eventOff="blur">
-                            <span>Intercept of the regression.</span>
-                        </ReactTooltip>
-                        <NumericField
-                            id={'alpha'}
-                            className={
-                                'form-control form-control-sm numeric-field'}
-                            min={-1}
-                            max={1}
-                            step={0.01}
-                            value={math.round(alpha, 2)}
-                            onChange={handleAlpha} />
-                    </div>
-                    <div className={'form-row'}>
-                        <div style={{ height: '50px', width: '100%'}}>
-                            <Rheostat
-                                min={-100}
-                                max={100}
-                                values={[math.round(alpha * 100, 2)]}
-                                onValuesUpdated={(sliderState) => {
-                                    handleAlpha(
-                                        (sliderState.values[0] * 0.01));
-                                }} />
-                        </div>
-                    </div>
-                    <div className={'form-row'}>
-                        <label htmlFor={'slope'}>
-                            <InlineMath>
-                                {String.raw`\beta_1`}
-                            </InlineMath>
-                        </label>
-                        <span className="help-tooltip"
-                            tabIndex="0"
-                            data-tip
-                            data-for="beta-tt">
-                            <sup>
-                                <i className="fas fa-question-circle"></i>
-                            </sup>
-                        </span>
-                        <ReactTooltip id="beta-tt" event="focus"
-                            eventOff="blur">
-                            <span>Slope of the regression.</span>
-                        </ReactTooltip>
-                        <NumericField
-                            id={'beta'}
-                            className={
-                                'form-control form-control-sm numeric-field'}
-                            min={-1}
-                            max={1}
-                            value={math.round(beta, 2)}
-                            step={0.01}
-                            onChange={handleBeta} />
-                    </div>
-                    <div className={'form-row'}>
-                        <div style={{ height: '50px', width: '100%'}}>
-                            <Rheostat
-                                min={-100}
-                                max={100}
-                                values={[math.round(beta * 100, 2)]}
-                                onValuesUpdated={(sliderState) => {
-                                    handleBeta(
-                                        (sliderState.values[0] * 0.01));
-                                }} />
-                        </div>
-                    </div>
-                    <div className={'form-row'}>
-                        <label htmlFor={'std-dev'}>
-                            Standard Deviation:
-                        </label>
-                        <NumericField
-                            id={'std-dev'}
-                            className={'form-control form-control-sm'}
-                            min={0.2}
-                            max={1}
-                            step={0.01}
-                            value={stdDev}
-                            onChange={handleStdDev} />
-                    </div>
-                    <div className={'form-row'}>
-                        <div style={{ height: '50px', width: '100%'}}>
-                            <Rheostat
-                                min={20}
-                                max={100}
-                                values={[(stdDev * 100)]}
-                                onValuesUpdated={(sliderState) => {
-                                    handleStdDev(
-                                        (sliderState.values[0] * 0.01));
-                                }} />
-                        </div>
-                    </div>
-                    <div className="form-row">
-                        <input className="btn btn-primary btn-block"
-                            disabled={seed ? false : true}
-                            id="generate-population"
-                            type="submit"
-                            value="Generate Population"/>
-                    </div>
-                </div>}
-            </fieldset>
-        </form>
-        { hasPopulation &&
-            <form onSubmit={(e) => {e.preventDefault();}}>
+        <div className={'sticky-top'}>
+            <form onSubmit={handleGenPop}
+                className="needs-validation clt-least-squares-form" noValidate >
                 <fieldset>
+                    <div className="form-group">
+                        <div className="form-row">
+                            <label htmlFor="seed" className="float-right">
+                                Seed:</label>
+                            <span className="help-tooltip"
+                                tabIndex="0"
+                                data-tip
+                                data-for="seed-tt">
+                                <sup>
+                                    <i className="fas fa-question-circle"></i>
+                                </sup>
+                            </span>
+                            <ReactTooltip id="seed-tt" event="focus"
+                                eventOff="blur">
+                                <span>Enter a different value to see a
+                                    different simulation</span>
+                            </ReactTooltip>
+                        </div>
+                        <div>
+                            <input type="text"
+                                id="seed"
+                                className={
+                                    seed ? 'form-control is-valid' :
+                                        'form-control' }
+                                value={seed}
+                                onChange={hndlSeed}
+                                placeholder={
+                                    'Enter a seed to start the simulation'}
+                                autoFocus required/>
+                        </div>
+                    </div>
+                    { seed &&
                     <div className={'form-group'}>
                         <div className={'form-row'}>
-                            <label htmlFor={'sample'}>
-                                Sample:
+                            <label htmlFor={'alpha'}>
+                                <InlineMath>
+                                    {String.raw`\beta_0`}
+                                </InlineMath>
                             </label>
+                            <span className="help-tooltip"
+                                tabIndex="0"
+                                data-tip
+                                data-for="alpha-tt">
+                                <sup>
+                                    <i className="fas fa-question-circle"></i>
+                                </sup>
+                            </span>
+                            <ReactTooltip id="alpha-tt" event="focus"
+                                eventOff="blur">
+                                <span>Intercept of the regression.</span>
+                            </ReactTooltip>
                             <NumericField
-                                id={'sample'}
-                                className={'form-control form-control-sm'}
-                                min={0}
-                                max={99}
-                                value={sampleIdx}
-                                step={1}
-                                onChange={handleSampleIdx} />
+                                id={'alpha'}
+                                className={'form-control form-control-sm \
+                                    numeric-field'}
+                                min={-1}
+                                max={1}
+                                step={0.01}
+                                value={math.round(alpha, 2)}
+                                onChange={handleAlpha} />
                         </div>
                         <div className={'form-row'}>
                             <div style={{ height: '50px', width: '100%'}}>
                                 <Rheostat
-                                    min={0}
-                                    max={99}
-                                    values={[sampleIdx]}
+                                    min={-100}
+                                    max={100}
+                                    values={[math.round(alpha * 100, 2)]}
                                     onValuesUpdated={(sliderState) => {
-                                        handleSampleIdx(sliderState.values[0]);
+                                        handleAlpha(
+                                            (sliderState.values[0] * 0.01));
                                     }} />
                             </div>
                         </div>
-                    </div>
+                        <div className={'form-row'}>
+                            <label htmlFor={'slope'}>
+                                <InlineMath>
+                                    {String.raw`\beta_1`}
+                                </InlineMath>
+                            </label>
+                            <span className="help-tooltip"
+                                tabIndex="0"
+                                data-tip
+                                data-for="beta-tt">
+                                <sup>
+                                    <i className="fas fa-question-circle"></i>
+                                </sup>
+                            </span>
+                            <ReactTooltip id="beta-tt" event="focus"
+                                eventOff="blur">
+                                <span>Slope of the regression.</span>
+                            </ReactTooltip>
+                            <NumericField
+                                id={'beta'}
+                                className={'form-control form-control-sm \
+                                    numeric-field'}
+                                min={-1}
+                                max={1}
+                                value={math.round(beta, 2)}
+                                step={0.01}
+                                onChange={handleBeta} />
+                        </div>
+                        <div className={'form-row'}>
+                            <div style={{ height: '50px', width: '100%'}}>
+                                <Rheostat
+                                    min={-100}
+                                    max={100}
+                                    values={[math.round(beta * 100, 2)]}
+                                    onValuesUpdated={(sliderState) => {
+                                        handleBeta(
+                                            (sliderState.values[0] * 0.01));
+                                    }} />
+                            </div>
+                        </div>
+                        <div className={'form-row'}>
+                            <label htmlFor={'std-dev'}>
+                                Standard Deviation:
+                            </label>
+                            <NumericField
+                                id={'std-dev'}
+                                className={'form-control form-control-sm'}
+                                min={0.2}
+                                max={1}
+                                step={0.01}
+                                value={stdDev}
+                                onChange={handleStdDev} />
+                        </div>
+                        <div className={'form-row'}>
+                            <div style={{ height: '50px', width: '100%'}}>
+                                <Rheostat
+                                    min={20}
+                                    max={100}
+                                    values={[(stdDev * 100)]}
+                                    onValuesUpdated={(sliderState) => {
+                                        handleStdDev(
+                                            (sliderState.values[0] * 0.01));
+                                    }} />
+                            </div>
+                        </div>
+                        <div className="form-row">
+                            <input className="btn btn-primary btn-block"
+                                disabled={seed ? false : true}
+                                id="generate-population"
+                                type="submit"
+                                value="Generate Population"/>
+                        </div>
+                    </div>}
                 </fieldset>
             </form>
-        }
-        </>
+            { hasPopulation &&
+                <form onSubmit={(e) => {e.preventDefault();}}>
+                    <fieldset>
+                        <div className={'form-group'}>
+                            <div className={'form-row'}>
+                                <label htmlFor={'sample'}>
+                                    Sample:
+                                </label>
+                                <NumericField
+                                    id={'sample'}
+                                    className={'form-control form-control-sm'}
+                                    min={0}
+                                    max={99}
+                                    value={sampleIdx}
+                                    step={1}
+                                    onChange={handleSampleIdx} />
+                            </div>
+                            <div className={'form-row'}>
+                                <div style={{ height: '50px', width: '100%'}}>
+                                    <Rheostat
+                                        min={0}
+                                        max={99}
+                                        values={[sampleIdx]}
+                                        onValuesUpdated={(sliderState) => {
+                                            handleSampleIdx(
+                                                sliderState.values[0]);
+                                        }} />
+                                </div>
+                            </div>
+                        </div>
+                    </fieldset>
+                </form>
+            }
+        </div>
     );
 };
 
