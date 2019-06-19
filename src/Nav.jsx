5286307609f642032b4e85eddc0b834619a9689b
Nick Buonincontri <nbuonin@gmail.com>
2019-06-19 16:43:49 -0400

    COJ-352 Add viewport warnings, responsive site name

--- src/Nav.jsx
+++ src/Nav.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import {Link} from 'react-router-dom';
 
 export const Nav = () => (
+    <>
     <nav className="navbar navbar-expand-lg navbar-dark">
         <Link className="navbar-brand" to="/">
             <span className="stats">Stats</span>
@@ -18,4 +19,12 @@ export const Nav = () => (
             </ul>
         </div>
     </nav>
+    <div className={'container'}>
+        <div className="alert alert-danger d-block d-xl-none view-only"
+            role="alert" aria-hidden="true">
+            Stats Interactives are optimized for larger screens. Please
+            switch to a desktop computer, or expand your browser size.
+        </div>
+    </div>
+    </>
 );
